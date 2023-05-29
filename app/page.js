53dd7a05dd293cb2463d4bc3464570df49556a79
Adnan Niaz <sanixstudio@gmail.com>
2023-05-29 11:34:58 -0700

    feat: added framer motion and add minor changes

--- app/page.js
+++ app/page.js
@@ -3,10 +3,12 @@ import { About, Contact, Header, Work } from "@/components";
 import Hero from "@/components/Hero/Hero";
 import { Flowbite } from "flowbite-react";
 import { customTheme } from "@/theme";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <Flowbite theme={{ customTheme }}>
+      {/* // <NextUIProvider> */}
       <Header />
       <main className="snap-y snap-mandatory h-screen overflow-scroll scroll-bar-hide max-w-[1220px] mx-auto bg-gray-5-- backdrop-opacity-30">
         <Hero />
@@ -14,6 +16,7 @@ export default function Home() {
         <Work />
         <Contact />
       </main>
+      {/* // </NextUIProvider> */}
     </Flowbite>
   );
 }
--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="sticky w-full max-w-[1220px] flex-row-between p-4 items-center justify-between mx-auto">
+    <div className="sticky w-full max-w-[1220px] flex-row-between p-4 items-center  mx-auto">
       <a href="/">
         <button className="rounded-full bg-teal-700 bg-gradient-to-r from-purple-500 to-pink-500 p-1 w-[50px] h-[50px]">
           <h1 className="text-2xl md:text-3xl">AN</h1>
@@ -23,7 +23,20 @@ const Header = () => {
           <a href="#contact">Contact</a>
         </li>
       </ul>
-      <div className=""></div>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Work</a>
+        </li>
+        <li>
+          <a href="/">Contact</a>
+        </li>
+      </ul>
     </div>
   );
 };
