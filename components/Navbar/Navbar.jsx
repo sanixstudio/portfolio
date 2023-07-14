5cd6bd56eaf8c7bb5875776a0f7988ea7d33428b
Adnan Niaz <sanixstudio@gmail.com>
2023-07-13 18:59:14 -0700

    fix: fixed pathname and Nav links issue

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,26 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import { BiMenuAltLeft } from "react-icons/bi";
-import useWindowSize from "@/hooks/useWindowSize";
+import Link from "next/link";
 
-const NavLink = ({ title, href, active }) => {
+const NavLink = ({ title, href }) => {
+  const pathname = usePathname();
+  const active = pathname === href;
+
   return (
     <li>
-      <a
-        href={href}
-        className={`text-${active ? "gray-300 font-bold" : "gray-500"}`}
-        aria-current={active ? "page" : undefined}
-      >
-        {title}
-      </a>
+      <Link href={href}>
+        <span className={`text-${active ? "gray-300 font-bold" : "gray-500"}`}>
+          {title}
+        </span>
+      </Link>
     </li>
   );
 };
 
 const MainNav = ({ isMobile }) => {
-  const path = usePathname();
-  const pageName = path.split("/")[1];
-
   return (
     <ul
       className={`flex flex-col gap-5 ${
@@ -29,17 +27,17 @@ const MainNav = ({ isMobile }) => {
           : "text-center"
       }`}
     >
-      <NavLink active={"" === pageName} title="Home" href="/" />
-      <NavLink active={"about" === pageName} title="About" href="about" />
-      <NavLink active={"work" === pageName} title="Work" href="work" />
-      <NavLink active={"contact" === pageName} title="Contact" href="contact" />
+      <NavLink title="Home" href="/" />
+      <NavLink title="About" href="/about" />
+      <NavLink title="Work" href="/work" />
+      <NavLink title="Contact" href="/contact" />
     </ul>
   );
 };
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
-  const { width } = useWindowSize();
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
   const toggleMenu = () => {
     setShowMenu((prevState) => !prevState);
@@ -47,6 +45,7 @@ const Navbar = () => {
 
   useEffect(() => {
     const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
       if (window.innerWidth >= 768) {
         setShowMenu(false);
       }
@@ -58,8 +57,6 @@ const Navbar = () => {
     };
   }, []);
 
-  const isMobile = width < 768;
-
   return (
     <header>
       <nav
@@ -69,9 +66,9 @@ const Navbar = () => {
       >
         <div className="flex justify-between items-center h-16 px-4 md:hidden">
           {!isMobile && (
-            <a href="/">
+            <Link href="/">
               <span className="text-4xl whitespace-nowrap">AN</span>
-            </a>
+            </Link>
           )}
           <button
             className="text-2xl focus:outline-none"
@@ -81,20 +78,18 @@ const Navbar = () => {
             <BiMenuAltLeft size={32} />
           </button>
         </div>
-        <div className={showMenu ? "block" : "hidden"}>
-          {showMenu && (
-            <div className="flex flex-col items-center justify-center h-screen">
-              <MainNav isMobile={isMobile} />
-            </div>
-          )}
-        </div>
+        {showMenu && (
+          <div className="flex flex-col items-center justify-center h-screen">
+            <MainNav isMobile={isMobile} />
+          </div>
+        )}
       </nav>
       <nav className="hidden md:flex fixed h-full z-50">
         <div className="flex flex-col flex-wrap items-between justify-between h-full mx-auto p-4">
           {!isMobile && (
-            <a href="/">
+            <Link href="/">
               <span className="text-4xl whitespace-nowrap">AN</span>
-            </a>
+            </Link>
           )}
           <div className="flex-1 max-h-[40%] flex justify-center">
             <div className="bg-gray-600 w-[1px]"></div>
